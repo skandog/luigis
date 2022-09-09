2c4e3efc6474cc943b2ab6568d3a76d0604113d8
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-09-09 14:56:24 +0100

    modal working

--- components/menu_item.js
+++ components/menu_item.js
@@ -4,35 +4,68 @@ import {
   Container,
   LinkOverlay,
   Image,
-  Button
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Lorem
 } from '@chakra-ui/react'
+import { useDisclosure } from '@chakra-ui/react'
+import { cartContext } from '../components/contexts/contexts'
+import { useContext } from 'react'
 
-const MenuItem = ({ children, href, title, thumbnail }) => {
+const MenuItem = ({ children, href, title, thumbnail, onClick, item }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { addToCart } = useContext(cartContext)
   return (
-    <Box w="100%" textAlign="left">
-      <Container
-        display="flex"
-        p={3}
-        maxW="container.lg"
-        wrap="wrap"
-        align="center"
-        justify="space-between"
-      >
-        <Image
-          src={thumbnail}
-          alt={title}
-          className="menu-item-thumbnail"
-          placeholder="blur"
-          loading="lazy"
-          w={20}
-        />
-        <Text mt={2}>{title}</Text>
-        <Button p={2} marginX={1}>
-          Add to cart
-        </Button>
-        <Button>More info</Button>
-      </Container>
-    </Box>
+    <>
+      <Box w="100%" textAlign="left">
+        <Container
+          display="flex"
+          p={3}
+          maxW="container.lg"
+          wrap="wrap"
+          align="center"
+          justify="space-between"
+          onClick={onOpen}
+        >
+          <Image
+            src={thumbnail}
+            alt={title}
+            className="menu-item-thumbnail"
+            placeholder="blur"
+            loading="lazy"
+            w={20}
+          />
+          <Text mt={2}>{title}</Text>
+          <Button p={2} marginX={1}>
+            Add to cart
+          </Button>
+          <Button>More info</Button>
+        </Container>
+      </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Description description description</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost" onClick={() => addToCart(item)}>
+              Add to basket{' '}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
 
--- pages/order.js
+++ pages/order.js
@@ -27,6 +27,7 @@ const Order = () => {
             key={index}
             thumbnail={item.image}
             alt={item.title}
+            item={item}
           />
         )
       })}
