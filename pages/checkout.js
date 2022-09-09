d4ed042e368ba93a32c2c1c9fbeecb85c52ccac7
brycen <brycensoc@gmail.com>
2022-09-09 14:07:25 +0100

    can buy items now, checkout cant be clicked unless two items are in cart

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,5 +1,42 @@
+import { useContext } from 'react'
+import { cartContext, boughtContext } from '../components/contexts/contexts'
+import Router from 'next/router'
 const Checkout = () => {
-  return <div>Check you out!</div>
+  const { cart, setCart } = useContext(cartContext)
+  const { updateBoughtItems } = useContext(boughtContext)
+  console.log(cart)
+  function handleCheckout() {
+    for (let i = 0; i < cart.length; i++) {
+      console.log(cart[i])
+      updateBoughtItems(cart[i])
+    }
+    setCart([])
+    Router.push('/')
+  }
+  return (
+    <main style={{ display: 'flex' }}>
+      <section>
+        <form style={{ display: 'flex', flexDirection: 'column' }}>
+          <input placeholder="First Name"></input>
+          <input placeholder="Last Name"></input>
+          <input placeholder="Address"></input>
+          <input placeholder="Postcode"></input>
+          <input placeholder="Email" type="email"></input>
+          <input placeholder="Phone number"></input>
+        </form>
+      </section>
+      <section>
+        {cart.map((item, index) => {
+          return (
+            <div key={index}>
+              {item.title} - Qty: {item.count}
+            </div>
+          )
+        })}
+        <button onClick={handleCheckout}>Buy!</button>
+      </section>
+    </main>
+  )
 }
 
 export default Checkout
